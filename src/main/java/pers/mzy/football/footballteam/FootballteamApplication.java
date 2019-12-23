63e3854fb14542497f8922918207aa9090cdaf84
mzy <lewis8074@163.com>
2019-12-23 14:26:31 +0800

    edit

--- src/main/java/pers/mzy/football/footballteam/FootballteamApplication.java
+++ src/main/java/pers/mzy/football/footballteam/FootballteamApplication.java
@@ -8,6 +8,7 @@ public class FootballteamApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FootballteamApplication.class, args);
+        System.out.println("启动成功");
     }
 
 }
